937864cf26acc3dd16cf663b73e00658c2116e7d
sinkeshkumar <sinkeshkumar164@gmail.com>
2025-10-05 22:32:48 +0530

    addedd

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
     cors({
         origin: "*",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowedHeaders: ["Content-Type"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
 );
